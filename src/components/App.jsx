4ec15ab771d969f3228aac3c184ea1afd90e16a0
akoval25 <88685936+akoval25@users.noreply.github.com>
2022-08-21 20:40:15 +0300

    profileCard

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,18 @@
+import ProfileCard from "./profileCard";
+import user from 'user';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+<ProfileCard 
+  avatar={user.avatar}
+  username={user.username}
+  tag={user.tag}
+  location={user.location}
+  followers={user.stats.followers}
+  views={user.stats.views}
+  likes={user.stats.likes}
+/>
     </div>
   );
 };
